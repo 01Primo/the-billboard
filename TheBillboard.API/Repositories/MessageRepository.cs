dd45e83b171ea083f713e8b79427b647413d5156
Doriano De Iaco <dorianblack97@gmail.com>
2022-03-28 10:37:50 +0200

    Add Postman collection with Test

--- TheBillboard.API/Repositories/MessageRepository.cs
+++ TheBillboard.API/Repositories/MessageRepository.cs
@@ -88,21 +88,6 @@ public class MessageRepository : IMessageRepository
                                  FROM Message M JOIN Author A ON A.Id = M.AuthorId                               
                                  WHERE M.Id=@Id";
 
-        //"SELECT M.Id," +
-        //                    " M.Title" +
-        //                    ", M.Body" +
-        //                    ", M.AuthorId" +
-        //                    ", A.Name" +
-        //                    ", A.Surname" +
-        //                    ", A.Mail as Email" +
-        //                    ", M.CreatedAt as MessageCreatedAt" +
-        //                    ", M.UpdatedAt as MessageUpdatedAt" +
-        //                    ", A.CreatedAt as AuthorCreatedAt" +
-        //                    " " +
-        //                    "FROM Message M JOIN Author A                           ON A.Id = M.AuthorId" +
-        //                    " " +
-        //                    "WHERE M.Id=@Id";
-
         return await _reader.GetByIdAsync<Message>(query, id);
     }
 
